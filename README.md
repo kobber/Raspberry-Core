c6dba7abd26672270423b36d0a3f725108b53dc1
Cassian Godsted <cassian@godsted.com>
2025-01-10 10:48:19 -0500

    Add Oreganized

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ This is a collection of minor tweaks for the Raspberry Flavoured modpack, focusi
 
 Comfortable Campfires is a client and server mod for Forge 1.19.2.
 - [Farmer's Delight](https://modrinth.com/mod/farmers-delight) is required.
-- [Twigs](https://modrinth.com/mod/twigs) is required.
-- [Supplementaries](https://modrinth.com/mod/supplementaries) is required.
+- [Twigs](https://modrinth.com/mod/twigs) is recommended.
+- [Supplementaries](https://modrinth.com/mod/supplementaries) is recommended.
+- [Oreganized](https://modrinth.com/mod/oreganized) is recommended.
 - [Caverns and Chasms](https://modrinth.com/mod/caverns-and-chasms) is optional.
 - [Cloth Config](https://modrinth.com/mod/cloth-config) is recommended to configure the mod.
 
@@ -17,14 +18,23 @@ Comfortable Campfires is a client and server mod for Forge 1.19.2.
 ## Additions
 
 ### Blocks
-- Ash Ovens, crafted from Supplementaries Ash Bricks
-- Silt Ovens, crafted from Twigs Silt Bricks
+- Ash Ovens, crafted from Supplementaries Ash Bricks.
+- Silt Ovens, crafted from Twigs Silt Bricks.
+- Lead Grate, crafted from Oreganized Lead Blocks
 
 ### Compatibility Changes
 - Twigs can now light Stoves.
 
 ### Configurable Tweaks
-- Stoves (Farmer's Delight) start unlit
-- Campfires start unlit
-- Braziers (Caverns and Chasms) start unlit 
-- World version is hidden
+- Stoves (Farmer's Delight) start unlit.
+- Campfires start unlit.
+- Braziers (Caverns and Chasms) start unlit. 
+- World version is hidden.
+
+## License
+[![Asset license (Unlicensed)](https://img.shields.io/badge/assets%20license-All%20Rights%20Reserved-red.svg?style=flat-square)](https://en.wikipedia.org/wiki/All_rights_reserved)
+[![Code license (MIT)](https://img.shields.io/badge/code%20license-MIT-green.svg?style=flat-square)](https://github.com/cassiancc/Raspberry-Core/blob/main/LICENSE.txt)
+
+
+
+If you are thinking about using the code or assets from Raspberry Core, please note the mod's licensing. **All assets of Raspberry Core are unlicensed and all rights are reserved to them by their respective authors.** The source code of Raspberry Flavoured mod for Minecraft 1.19.2 is under the MIT license.
--- src/main/java/cc/cassian/raspberry/registry/RaspberryBlocks.java
+++ src/main/java/cc/cassian/raspberry/registry/RaspberryBlocks.java
@@ -4,7 +4,10 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,6 +16,7 @@ import oshi.util.tuples.Pair;
 import vectorwing.farmersdelight.FarmersDelight;
 import vectorwing.farmersdelight.common.block.StoveBlock;
 import vectorwing.farmersdelight.common.registry.ModBlocks;
+import xanthian.copperandtuffbackport.blocks.custom.GrateBlock;
 
 import java.util.function.Supplier;
 
@@ -20,15 +24,24 @@ import static cc.cassian.raspberry.RaspberryMod.MOD_ID;
 
 public class RaspberryBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
-    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> SILT_STOVE = registerBlock("silt_stove", ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
-    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> ASH_STOVE = registerBlock("ash_stove", ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
+
+    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>>
+            SILT_STOVE = registerBlock("silt_stove",
+            ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
+
+    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>>
+            ASH_STOVE = registerBlock("ash_stove",
+            ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
+
+    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>>
+            LEAD_GRATE = registerBlock("lead_grate",
+            ()-> new GrateBlock(BlockBehaviour.Properties.of(Material.HEAVY_METAL).noOcclusion().strength(5.0F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.METAL)), CreativeModeTab.TAB_BUILDING_BLOCKS);
 
 
     public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> registerBlock(String blockID, Supplier<Block> blockSupplier, CreativeModeTab tab) {
         final var block = BLOCKS.register(blockID, blockSupplier);
         final var item = RaspberryItems.ITEMS.register(blockID, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
         return new Pair<>(block, item);
-
     }
 
     /**
