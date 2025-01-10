21fb6c0a9ba0c3eaed1a35f49ba7922c112c11e9
Cassian Godsted <cassian@godsted.com>
2025-01-09 20:58:50 -0500

    Save blockitem

--- src/main/java/cc/cassian/raspberry/registry/RaspberryBlocks.java
+++ src/main/java/cc/cassian/raspberry/registry/RaspberryBlocks.java
@@ -4,11 +4,11 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import oshi.util.tuples.Pair;
 import vectorwing.farmersdelight.FarmersDelight;
 import vectorwing.farmersdelight.common.block.StoveBlock;
 import vectorwing.farmersdelight.common.registry.ModBlocks;
@@ -19,14 +19,14 @@ import static cc.cassian.raspberry.RaspberryMod.MOD_ID;
 
 public class RaspberryBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
-    public static RegistryObject<Block> SILT_STOVE = registerBlock("silt_stove", ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
-    public static RegistryObject<Block> ASH_STOVE = registerBlock("ash_stove", ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
+    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> SILT_STOVE = registerBlock("silt_stove", ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
+    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> ASH_STOVE = registerBlock("ash_stove", ()-> new StoveBlock(BlockBehaviour.Properties.copy(ModBlocks.STOVE.get())), FarmersDelight.CREATIVE_TAB);
 
 
-    public static RegistryObject<Block> registerBlock(String blockID, Supplier<Block> blockSupplier, CreativeModeTab tab) {
+    public static Pair<RegistryObject<Block>, RegistryObject<BlockItem>> registerBlock(String blockID, Supplier<Block> blockSupplier, CreativeModeTab tab) {
         final var block = BLOCKS.register(blockID, blockSupplier);
         final var item = RaspberryItems.ITEMS.register(blockID, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
-        return block;
+        return new Pair<>(block, item);
 
     }
 }
