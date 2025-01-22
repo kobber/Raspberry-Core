d00859aca20c873c1f8707dceaa8cba9adc60349
DavigJ <david.j.jung@gmail.com>
2025-01-22 10:03:00 +0400

    braziers no longer crackle while unlit

--- src/main/java/cc/cassian/raspberry/mixin/BrazierBlockMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/BrazierBlockMixin.java
@@ -1,12 +1,19 @@
 package cc.cassian.raspberry.mixin;
 
 import cc.cassian.raspberry.config.ModConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.teamabnormals.caverns_and_chasms.common.block.BrazierBlock;
 
@@ -15,6 +22,8 @@ import static vectorwing.farmersdelight.common.block.StoveBlock.LIT;
 @Pseudo
 @Mixin(BrazierBlock.class)
 public abstract class BrazierBlockMixin {
+    @Shadow @Final public static BooleanProperty LIT;
+
     @Inject(
             method = "getStateForPlacement",
             at = @At(value = "RETURN"),
@@ -22,5 +31,14 @@ public abstract class BrazierBlockMixin {
     private void startStovesLit(BlockPlaceContext context, CallbackInfoReturnable<BlockState> cir) {
         if (!ModConfig.get().braziersStartLit)
             cir.setReturnValue(cir.getReturnValue().setValue(LIT, false));
+    }
+
+    @Inject(method = "animateTick",
+    at = @At("HEAD"),
+    cancellable = true)
+    private void hush(BlockState state, Level level, BlockPos pos, RandomSource random, CallbackInfo ci) {
+        if (!state.getValue(LIT)) {
+            ci.cancel();
+        }
     }
 }
