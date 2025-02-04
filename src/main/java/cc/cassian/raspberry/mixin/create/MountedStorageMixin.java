832f62a184a42a2dee09cb5c4419bd998ec624be
Cassian Godsted <cassian@godsted.com>
2025-02-04 18:03:23 -0500

    Fix typo

--- src/main/java/cc/cassian/raspberry/mixin/create/MountedStorageMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/create/MountedStorageMixin.java
@@ -18,7 +18,7 @@ public class MountedStorageMixin {
     @Inject(method = "canUseAsStorage", at = @At(value = "HEAD"), remap = false, cancellable = true)
     private static void moddedBlocksAreStorage(BlockEntity be, CallbackInfoReturnable<Boolean> cir) {
         var modlist = ModList.get();
-        if (modlist.isLoaded("farmerdelight"))
+        if (modlist.isLoaded("farmersdelight"))
             if (FarmersDelightCompat.isCabinet(be))
                 cir.setReturnValue(true);
         else if (modlist.isLoaded("another_furniture"))
