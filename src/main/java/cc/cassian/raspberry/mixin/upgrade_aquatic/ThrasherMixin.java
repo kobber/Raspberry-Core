6b0eb2ee81d598fee22cf39b8eaae55dae445838
Cassian Godsted <cassian@godsted.com>
2025-02-23 11:27:43 -0500

    Further thrasher fixes

--- src/main/java/cc/cassian/raspberry/mixin/upgrade_aquatic/ThrasherMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/upgrade_aquatic/ThrasherMixin.java
@@ -1,7 +1,11 @@
 package cc.cassian.raspberry.mixin.upgrade_aquatic;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.teamabnormals.upgrade_aquatic.common.entity.monster.Thrasher;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.material.Fluid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,6 +26,18 @@ public class ThrasherMixin {
     @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lcom/teamabnormals/upgrade_aquatic/common/entity/monster/Thrasher;getPassengers()Ljava/util/List;"))
     private List<Entity> tickMixin(Thrasher instance) {
         return Collections.singletonList(instance.getFirstPassenger());
+    }
+
+    @WrapOperation(
+            method = "tick",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z")
+    )
+    private boolean bypassTickNullPointer(Entity instance, TagKey<Fluid> fluidTag, Operation<Boolean> original) {
+        if (instance == null) {
+            return false;
+        } else {
+            return original.call(instance, fluidTag);
+        }
     }
 
 }
