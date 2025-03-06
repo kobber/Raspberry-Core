cf512278211979b8fe8be8daebb7ddda32c3463c
Cassian Godsted <cassian@godsted.com>
2025-03-06 14:30:41 -0500

    further safety checks

--- src/main/java/cc/cassian/raspberry/mixin/create/FanProcessingMixin.java
+++ src/main/java/cc/cassian/raspberry/mixin/create/FanProcessingMixin.java
@@ -1,22 +1,14 @@
 package cc.cassian.raspberry.mixin.create;
 
-import cc.cassian.raspberry.config.ModConfig;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.kinetics.fan.processing.FanProcessing;
 import com.simibubi.create.content.kinetics.fan.processing.FanProcessingType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Collections;
 import java.util.List;
 
 @Mixin(FanProcessing.class)
@@ -27,7 +19,7 @@ public class FanProcessingMixin {
         try {
             assert stacks != null;
             stacks.remove(0);
-        } catch (UnsupportedOperationException exception) {
+        } catch (Exception exception) {
             cir.setReturnValue(false);
         }
     }
