f80a07d3e82215d56235381b52ad8fede91c00db
Kobber <kobber@users.noreply.github.com>
2025-11-05 17:45:34 +0100

    decreased garland particles and disable per user settings (#44)
    Co-authored-by: Cassian Godsted <cassian@godsted.com>

--- src/main/java/cc/cassian/raspberry/client/RaspberryModClient.java
+++ src/main/java/cc/cassian/raspberry/client/RaspberryModClient.java
@@ -1,26 +1,40 @@
 package cc.cassian.raspberry.client;
 
+import cc.cassian.raspberry.ModCompat;
 import cc.cassian.raspberry.RaspberryMod;
+import cc.cassian.raspberry.client.config.ModConfigFactory;
+import cc.cassian.raspberry.events.FlowerGarlandEvent;
 import cc.cassian.raspberry.registry.BlockSupplier;
 import cc.cassian.raspberry.client.registry.RaspberryItemProperties;
 import cc.cassian.raspberry.registry.RaspberryBlocks;
 import cc.cassian.raspberry.registry.RaspberryEntityTypes;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod.EventBusSubscriber(modid = RaspberryMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class RaspberryModClient {
+
+    public static void init(FMLJavaModLoadingContext context) {
+        // Register config
+        registerModsPage(context);
+        MinecraftForge.EVENT_BUS.addListener(RaspberryModClient::clickTick);
+    }
+
     @SubscribeEvent
     public static void registerBlockColors(RegisterColorHandlersEvent.Block event){
         BlockColors blockColors = event.getBlockColors();
@@ -49,6 +63,11 @@ public class RaspberryModClient {
         RaspberryItemProperties.register();
     }
 
+    // Forge event bus.
+    public static void clickTick(TickEvent.ClientTickEvent event) {
+        FlowerGarlandEvent.tick(Minecraft.getInstance());
+    }
+
     @SubscribeEvent
     public static void onModelBake(ModelEvent.RegisterAdditional event) {
         // This should probably be extracted into a handler of some kind
@@ -57,4 +76,14 @@ public class RaspberryModClient {
         event.register(RaspberryMod.locate("block/playful_wildflowers_potted"));
         event.register(RaspberryMod.locate("block/hopeful_wildflowers_potted"));
     }
+
+
+    /**
+     * Integrate Cloth Config screen (if mod present) with Forge mod menu.
+     */
+    public static void registerModsPage(FMLJavaModLoadingContext context) {
+        if (ModCompat.CLOTH_CONFIG)
+            context.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ModConfigFactory::createScreen));
+    }
+
 }
